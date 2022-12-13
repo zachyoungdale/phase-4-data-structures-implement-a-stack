21fbfb0984b4aaa74cb0d3ffa4a44e71ca3b2e8d
Zachary Youngdale <zyoungdale@gmail.com>
2022-12-13 15:57:14 -0500

    commit tree d6e3ceb0076239d6ec5818f78e593b5aaeb54189

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,18 @@
-# your code here
+class Stack
+
+    def initialize
+        @stack = [];
+    end
+
+    def push(value)
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+end# your code here
